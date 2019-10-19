2bb6a883c274c59a1beacc70b5ed7d36a77c59a2
xixiong <xixiong@iu.edu>
2019-10-19 19:51:07 -0400

    profile model added

--- api/models/profile.js
+++ api/models/profile.js
@@ -1,17 +1,20 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+const userProfileSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    userID: {
+    userID: { 
+        // this will be the connection with users, when user signup, a profile should be generated also
+        // you can get user's first name and last name from this
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         require: true,
         unique: true
-    },
-    profileIMG: String,
-    basicInfo: String,
-
+    }, 
+    profileIMG: String, //This is the image name of the user profile image. The actual image sistored in the profileImg
+    basicInfo: String, //This will be the user's info (bibliography).
+    tags: Array, // this should be a array of strings, each string represent a tag
+    major: String, // user's major
 });
 
 
-module.exports = mongoose.model('Profile', userSchema);
+module.exports = mongoose.model('Profile', userProfileSchema);
