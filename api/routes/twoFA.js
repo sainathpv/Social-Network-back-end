a6ddf71b98b26866326186f296871f881a401ae2
xixiong <xixiong@iu.edu>
2019-10-19 19:52:41 -0400

    fixed the 2 way auth error

--- api/routes/twoFA.js
+++ api/routes/twoFA.js
@@ -7,20 +7,20 @@ const checkAuth = require('../middleware/check-auth');
 
 // for login, after varified user's secret, we ask user for the 6 letter token on their phone
 router.post('/twoFALogin', checkAuth,  (req, res, next) => {
-    User.find({email: req.body.email})
+    User.findOne({email: req.body.email})
     .exec()
     .then(user => {
 
         // verifing if the user's secret is related to the 6 letter token from the user
         //this is still having some issue, I will go fix this later
         var verified = speakEasy.totp.verify({
-            secret: user[0].twoFASecret,
+            secret: user.twoFASecret,
             encoding: 'base32',
             token: req.body.token,
             window:1
         });
         
-        if(!verified){ //generate JWT token if 2FA is successful
+        if(verified){ //generate JWT token if 2FA is successful
             const token = jwt.sign({
                 name: user.name,
                 email: user.email,
