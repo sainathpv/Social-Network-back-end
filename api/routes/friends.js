dd4074050f6bc976eb852928938b4a2de31384ae
DishaTalreja3 <37664210+DishaTalreja3@users.noreply.github.com>
2019-11-18 10:42:29 -0500

    Add files via upload
    Backend routes for friends

---
+++ api/routes/friends.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+const Post = require('../models/post');
+const Friends = require('../models/friends');
+const Comment = require('../models/comment');
+const mongoose = require('mongoose');
+const check2Auth = require('../middleware/check-2auth');
+const fs = require('fs');
+
+const FriendsController = require('../controllers/friends');
+
+router.post('/createfriends', check2Auth, FriendsController.friends_create);
+router.post('/editfriends', check2Auth, FriendsController.friends_edit);
+
+router.get('/', check2Auth, FriendsController.friends_get);
+
+function findFriends(profileID) {
+  Friends.find({ profileID });
+}
