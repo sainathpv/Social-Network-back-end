6f5f9d0ddf615e46770ae9d5a8fea84760f3474c
xixiong <xixiong@iu.edu>
2019-12-07 23:21:46 -0500

    profile privacy and company profile page

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -120,33 +120,24 @@ exports.users_signup = async (req, res, next) => {
         // create a new user with the input information and hashed passsword
         var user;
 
-        if (req.body.accountType == "student") {
-            user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                name: name,
-                userName: userName,
-                email: email,
-                password: pwdHash,
-                twoFASecret: secret.base32,
-                authorization: false
-            });
-        } else {
-            user = new User({
-                _id: new mongoose.Types.ObjectId(),
-                company: company,
-                userName: req.body.userName,
-                email: email,
-                password: pwdHash,
-                twoFASecret: secret.base32,
-                authorization: false
-            });
-        }
+        user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            trueName: name,
+            userName: userName,
+            email: email,
+            password: pwdHash,
+            twoFASecret: secret.base32,
+            authorization: false
+        });
+
+        console.log(req.body);
+
 
         const profile = new Profile({
             _id: new mongoose.Types.ObjectId(),
             user: user._id,
             interests: ["questions"],
-            accountType: user.accountType,
+            accountType: req.body.accountType,
             name: user.userName,
         });
 
